4c23b15813305f70a7146048debe4e0409a1bdd5
YeisonGonz <yeisonrascado@gmail.com>
2025-08-04 20:05:59 +0200

    New Project

---
+++ src/test/java/internal/cdnapp/cdn/CdnApplicationTests.java
@@ -0,0 +1,13 @@
+package internal.cdnapp.cdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
