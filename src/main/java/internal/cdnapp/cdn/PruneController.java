209a273963c29b78538c3d8a264ce4f5bd7eda8c
YeisonGonz <yeisonrascado@gmail.com>
2025-08-06 22:38:24 +0200

    Added delete endpoint

---
+++ src/main/java/internal/cdnapp/cdn/PruneController.java
@@ -0,0 +1,65 @@
+package internal.cdnapp.cdn;
+
+import internal.cdnapp.cdn.components.PhotoStorageService;
+import internal.cdnapp.cdn.entity.CdnUrl;
+import internal.cdnapp.cdn.repository.CdnUrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/prune")
+public class PruneController {
+
+    private final CdnUrlRepository cdnUrlRepository;
+    private final PhotoStorageService photoStorageService;
+
+    @Autowired
+    public PruneController(final CdnUrlRepository cdnUrlRepository, final PhotoStorageService photoStorageService) {
+        this.cdnUrlRepository = cdnUrlRepository;
+        this.photoStorageService = photoStorageService;
+    }
+
+    public boolean deletePhoto( UUID uuid){
+        Optional<CdnUrl> cdnUrlOptional = cdnUrlRepository.findById(uuid);
+
+        if (cdnUrlOptional.isEmpty()) {
+            return false;
+        }
+
+        CdnUrl cdnUrl = cdnUrlOptional.get();
+        Path photoFile = photoStorageService.getRootLocation().resolve(cdnUrl.getFilePath());
+
+        try {
+            Files.deleteIfExists(photoFile);
+
+            Path dir = photoFile.getParent();
+            if (Files.isDirectory(dir) && Files.list(dir).findAny().isEmpty()) {
+                Files.delete(dir);
+            }
+            cdnUrlRepository.deleteById(uuid);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<String> delete(@PathVariable("uuid") UUID uuid) {
+        boolean deleted = deletePhoto(uuid);
+        if (deleted) {
+            return ResponseEntity.ok("photo deleted successfully");
+        } else {
+            return ResponseEntity.ok("failed to delete photo");
+        }
+    }
+}
